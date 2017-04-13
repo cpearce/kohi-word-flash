8451a08c0c0e7533c259ddc525be194854a2dae7
Chris Pearce <cpearce@mozilla.com>
2017-04-13 22:12:55 +1200

    Move words out into separate JS file.

---
+++ words.js
@@ -0,0 +1,66 @@
+var wordGroups = [
+  {
+    name: 'Magenta',
+    words: [
+      'a', 'am', 'and', 'at', 'big',
+      'can', 'come', 'Dad', 'going', 'he',
+      'help', 'here', 'I', 'in', 'is',
+      'it', 'like', 'look', 'me', 'Mum',
+      'my', 'no', 'not', 'said', 'the',
+      'to', 'up', 'was', 'we', 'went',
+      'will', 'with', 'you',
+    ]
+  },
+  {
+    name: 'Yellow',
+    words: [
+      'after', 'big', 'down', 'get out', 'have',
+      'help', 'here', 'like', 'little', 'my',
+      'not', 'over', 'past', 'play', 'put',
+      'see', 'she', 'they', 'this', 'to',
+      'under', 'up', 'we will', 'went', 'what',
+      'will', 'with', 'yellow', 'your',
+     ]
+  },
+  {
+    name: 'Red',
+    words: [
+      'are', 'away', 'can', 'down', 'going',
+      'had', 'he', 'his', 'home', 'into',
+      'my', 'on', 'over', 'said', 'see',
+      'shouted', 'some', 'thank you', 'the', 'this',
+      'to', 'under', 'us', 'want', 'we',
+      'went',
+     ]
+  },
+  {
+    name: 'Blue',
+    words: [
+      'did', 'eat', 'food', 'from', 'get',
+      'good', 'have', 'know', 'let', 'looked',
+      'make', 'makes', 'not', 'of', 'please',
+      'there', 'took', 'who',
+    ]
+  },
+  {
+    name: 'Green',
+    words: [
+      'about', 'again', 'around', 'away', 'book',
+      'but', 'came', 'day', 'drive', 'first',
+      'laugh', 'Miss', 'Mr', 'Mrs', 'old',
+      'our', 'ready', 'river', 'road', 'room',
+      'swim', 'teach', 'with', 'woman', 'yelled',
+      'your',
+    ]
+  },
+  {
+    name: 'Orange',
+    words: [
+      'about', 'after', 'again', 'because', 'can\'t',
+      'children', 'could', 'did', 'didn\'t', 'don\'t',
+      'goes', 'have', 'house', 'please', 'right',
+      'school', 'under', 'were','what', 'where',
+      'which', 'who',
+    ]
+  }
+];
