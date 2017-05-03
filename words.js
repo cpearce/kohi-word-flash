1e8f9217dcd2be35ba376bd0caac743bccd441cc
Chris Pearce <cpearce@mozilla.com>
2017-05-03 20:13:06 +1200

    Add new word groups.

--- words.js
+++ words.js
@@ -1,66 +1,194 @@
 var wordGroups = [
   {
-    name: 'Magenta',
+    name: "Magenta",
     words: [
-      'a', 'am', 'and', 'at', 'big',
-      'can', 'come', 'Dad', 'going', 'he',
-      'help', 'here', 'I', 'in', 'is',
-      'it', 'like', 'look', 'me', 'Mum',
-      'my', 'no', 'not', 'said', 'the',
-      'to', 'up', 'was', 'we', 'went',
-      'will', 'with', 'you',
+      "a", "am", "and", "at", "big",
+      "can", "come", "Dad", "going", "he",
+      "help", "here", "I", "in", "is",
+      "it", "like", "look", "me", "Mum",
+      "my", "no", "not", "said", "the",
+      "to", "up", "was", "we", "went",
+      "will", "with", "you",
     ]
   },
   {
-    name: 'Yellow',
+    name: "Yellow",
     words: [
-      'after', 'big', 'down', 'get out', 'have',
-      'help', 'here', 'like', 'little', 'my',
-      'not', 'over', 'past', 'play', 'put',
-      'see', 'she', 'they', 'this', 'to',
-      'under', 'up', 'we will', 'went', 'what',
-      'will', 'with', 'yellow', 'your',
+      "after", "big", "down", "get out", "have",
+      "help", "here", "like", "little", "my",
+      "not", "over", "past", "play", "put",
+      "see", "she", "they", "this", "to",
+      "under", "up", "we will", "went", "what",
+      "will", "with", "yellow", "your",
      ]
   },
   {
-    name: 'Red',
+    name: "Red",
     words: [
-      'are', 'away', 'can', 'down', 'going',
-      'had', 'he', 'his', 'home', 'into',
-      'my', 'on', 'over', 'said', 'see',
-      'shouted', 'some', 'thank you', 'the', 'this',
-      'to', 'under', 'us', 'want', 'we',
-      'went',
+      "are", "away", "can", "down", "going",
+      "had", "he", "his", "home", "into",
+      "my", "on", "over", "said", "see",
+      "shouted", "some", "thank you", "the", "this",
+      "to", "under", "us", "want", "we",
+      "went",
      ]
   },
   {
-    name: 'Blue',
+    name: "Blue",
     words: [
-      'did', 'eat', 'food', 'from', 'get',
-      'good', 'have', 'know', 'let', 'looked',
-      'make', 'makes', 'not', 'of', 'please',
-      'there', 'took', 'who',
+      "did", "eat", "food", "from", "get",
+      "good", "have", "know", "let", "looked",
+      "make", "makes", "not", "of", "please",
+      "there", "took", "who",
     ]
   },
   {
-    name: 'Green',
+    name: "Green",
     words: [
-      'about', 'again', 'around', 'away', 'book',
-      'but', 'came', 'day', 'drive', 'first',
-      'laugh', 'Miss', 'Mr', 'Mrs', 'old',
-      'our', 'ready', 'river', 'road', 'room',
-      'swim', 'teach', 'with', 'woman', 'yelled',
-      'your',
+      "about", "again", "around", "away", "book",
+      "but", "came", "day", "drive", "first",
+      "laugh", "Miss", "Mr", "Mrs", "old",
+      "our", "ready", "river", "road", "room",
+      "swim", "teach", "with", "woman", "yelled",
+      "your",
     ]
   },
   {
-    name: 'Orange',
+    name: "Orange 1",
     words: [
-      'about', 'after', 'again', 'because', 'can\'t',
-      'children', 'could', 'did', 'didn\'t', 'don\'t',
-      'goes', 'have', 'house', 'please', 'right',
-      'school', 'under', 'were','what', 'where',
-      'which', 'who',
+      "about", "after", "again", "because", "can't",
+      "children", "could", "did", "didn't", "don't",
+      "goes", "have", "house", "please", "right",
+      "school", "under", "were","what", "where",
+      "which", "who",
+    ]
+  },
+  {
+    name: "Orange 2",
+    words: [
+      "an", "as", "asleep", "away", "brother",
+      "cried", "home", "if", "laugh", "let",
+      "made", "making", "next", "night", "now",
+      "old", "our", "over", "right", "round",
+      "sister", "than",
+    ]
+  },
+  {
+    name: "Orange 3",
+    words: [
+      "bag", "cake", "coming", "end", "find",
+      "fun", "girl", "had", "help", "here’s",
+      "last", "let", "need", "next", "or",
+      "soon", "talked", "than", "very", "walk",
+      "what", "your",
+    ]
+  },
+  {
+    name: "Orange 4",
+    words: [
+      "always", "another", "bag", "best", "feel",
+      "good", "house", "just", "know", "left",
+      "love", "much", "old", "run", "school",
+      "still", "their", "them", "took", "use",
+      "walked", "want",
+    ]
+  },
+  {
+    name: "Orange 5",
+    words: [
+      "been", "dog", "eat", "everyone", "food",
+      "I’ll", "long", "man", "more", "other",
+      "shouted", "things", "think", "thought", "through",
+      "took", "tree", "two", "us", "wanted",
+      "way", "well",
+    ]
+  },
+  {
+    name: "Orange 6",
+    words: [
+      "animals", "bed", "began", "boy", "door",
+      "first", "found", "gave", "live", "lots",
+      "may", "must", "never", "red", "right",
+      "say", "sea", "something", "stop", "that’s",
+      "these", "work",
+    ]
+  },
+  {
+    name: "Orange 7",
+    words: [
+      "around", "better", "bring", "every", "fast",
+      "forgot", "garden", "head", "I’ve", "laughed",
+      "let’s", "many", "night", "only", "push",
+      "small", "suddenly", "three", "today", "told",
+      "town", "word",
+    ]
+  },
+  {
+    name: "Turqoise",
+    words: [
+      "car", "couldn’t", "king", "great", "why",
+      "cried", "keep", "room", "last", "jumped",
+      "even", "before", "gran", "clothes", "tell",
+      "key", "place", "boat", "window", "sleep",
+      "window", "sleep", "feet", "morning", "queen",
+      "each", "book", "its", "green", "different",
+      "let", "which", "inside", "any", "hat",
+      "snow", "air", "trees", "bad", "tea",
+      "top", "eyes", "fell", "friends", "box",
+      "dark", "grandad", "there’s", "better", "hot",
+      "sun", "across", "gone", "hard", "really",
+      "wind", "wish", "eggs", "once", "stopped",
+      "ever", "miss", "cold", "park", "live",
+      "birds", "duck", "horse", "rabbit", "white",
+      "he’s", "river", "does", "long", "shout",
+      "ball", "I’m"
+    ]
+  },
+  {
+    name: "Purple",
+    words: [
+      "giant", "looks", "along", "plants", "dragon",
+      "pulled", "we’re", "fly", "grow", "above",
+      "during", "themselves", "bigger", "heard", "brought",
+      "high", "turn", "change", "upon", "important",
+      "outside", "swimming", "family", "following", "together",
+      "finish", "happy", "tomorrow", "grandma", "uncle",
+      "grandpa", "aunty", "sometimes", "clock", "money",
+      "year", "already", "leave", "goodbye", "paper",
+      "life", "gone", "between", "having", "both",
+      "noise", "tries", "earth", "inside", "number",
+      "easily", "often", "enjoy", "lady", "onto",
+      "later", "won", "against", "dry", "anyone",
+      "excuse", "lucky", "answer", "fruit", "minutes",
+      "plenty", "biggest", "heavy", "mountain", "sacred",
+      "break", "icecream", "mouth", "throw", "tonight",
+      "centre", "lesson", "climb", "listen", "visit",
+      "deep", "live", "plant", "afraid", "able"
+    ]
+  },
+  {
+    name: "Gold",
+    words: [
+      "bathroom", "burst", "afternoon", "cushion", "afterwards",
+      "customer", "alike", "difficult", "alright", "dinosaur",
+      "buy", "banana", "face", "brown", "fastest",
+      "fault", "few", "tried", "wait", "gigantic",
+      "naughty", "photograph", "while", "pie", "piece",
+      "weird", "world", "replied", "you’re", "air",
+      "believe", "getting", "interesting", "accident", "boxes",
+      "happened", "invent", "add", "breakfast", "hello",
+      "invite", "altogether", "bubble", "shopping", "arrive",
+      "hope", "special", "awful", "beginning", "hoping",
+      "awake", "behind", "summer", "backwards", "calm",
+      "trouble", "become", "decided", "twelve", "bedroom",
+      "finally", "instead", "twenty", "ache", "blanket",
+      "though", "allow", "build", "quarter", "Monday",
+      "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+      "Sunday", "assembly", "somebody", "balance", "finished",
+      "ballet", "hole", "square", "hour", "barbeque",
+      "basket", "however", "knife", "actually", "activity",
+      "parents", "camera", "address", "blown", "careful",
+      "picked", "adventure", "question", "quietly", "hospital"
     ]
   }
 ];
